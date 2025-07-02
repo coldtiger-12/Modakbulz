78b02ce33fd14974ea2028826a08ba149f829e99
Shinhanyu <dpfm01@naver.com>
2025-07-02 16:09:41 +0900

    김연주

--- src/main/java/modackbulz/app/Application/web/HomeController.java
+++ src/main/java/modackbulz/app/Application/web/HomeController.java
@@ -21,14 +21,14 @@ public class HomeController {
   public String home(Model model) {
     // goCampingService.getBasedList()가 반환하는 Mono<List<...>>를
     // .block()을 호출하여 List<...>로 변환합니다.
-    // 메인에 표시할 캠핑장 개수를 4개로 지정합니다.
-    List<GoCampingDto.Item> campList = goCampingService.getBasedList(4).block();
+    // 메인에 표시할 캠핑장 개수를 8개로 지정합니다.
+    List<GoCampingDto.Item> campList = goCampingService.getBasedList(8).block();
 
     // 변환된 리스트를 모델에 추가합니다.
     model.addAttribute("recommendCamps", campList);
 
-    //커뮤니티 최근 글 5개 가져오기
-    List<Community> recentPosts = communityDAO.findRecentPosts(5);
+    //커뮤니티 최근 글 8개 가져오기
+    List<Community> recentPosts = communityDAO.findRecentPosts(8);
     model.addAttribute("recentPosts", recentPosts);
 
     return "index";
