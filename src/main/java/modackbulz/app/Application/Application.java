dba51c81e0f9bd86862349991bedb4f32a31318c
Hyun702 <plka5824@gmail.com>
2025-07-15 11:34:00 +0900

    2025-07-15

--- src/main/java/modackbulz/app/Application/Application.java
+++ src/main/java/modackbulz/app/Application/Application.java
@@ -1,14 +1,17 @@
 package modackbulz.app.Application;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling; // 👈 [추가] import
 
 @SpringBootApplication
 @EnableScheduling
+@EnableEncryptableProperties	// Jasypt 활성화 어노테이션
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
 }
